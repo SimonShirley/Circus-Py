691bd4b2893cbea53148deb8c3da5abf024a19b5
Simon Shirley <email@simonshirley.uk>
2026-07-23 22:51:15 +0100

    Re-generate balloon row when the row is depleted

--- circus.py
+++ circus.py
@@ -1,5 +1,6 @@
 # Example file showing a basic pygame "game loop"
 import pygame, acrobat, balloon, logging, console_logger
+import math
 
 # pygame setup
 pygame.init()
@@ -11,15 +12,36 @@ running: bool = True
 delta_time: float = 0
 player = acrobat.Acrobat(screen_width / 25, screen_width / 25, pygame.Vector2(0, 500))
 balloon_list: list[balloon.Balloon] = []
-balloon_line_count: int = 25
-balloon_width: float = screen_width / (balloon_line_count * 2)
+balloon_active_list: list[int] = []
+balloon_row_count: int = 3
+balloon_line_count: int = 26
+balloon_width: float = screen_width / ((balloon_line_count - 1) * 2)
 
-for i in range(0, 3):
-    for j in range(0, balloon_line_count + 1):
+for i in range(0, balloon_row_count):
+    balloon_active_list.append(balloon_line_count)
+
+    for j in range(0, balloon_line_count):
         balloon_list.append(balloon.Balloon((j * balloon_width * 2), (i * balloon_width * 2) + balloon_width, balloon_width, balloon_width))
 
 logger: logging.Logger = console_logger.Logger().get_logger()
 logger.debug('simple message')
+
+def decrease_balloon_row_count(balloon_index: int):
+    global balloon_line_count, balloon_active_list
+
+    balloon_row_index = math.floor(balloon_index / balloon_line_count)
+    balloon_active_list[balloon_row_index] -= 1
+
+def reactivate_balloon_row(row_index: int):
+    global balloon_list, balloon_active_list
+
+    balloon_row_offset: int = balloon_line_count * row_index
+ 
+    for balloon_index in range(balloon_row_offset, balloon_row_offset + balloon_line_count):
+        balloon_list[balloon_index].set_active(True)
+
+    balloon_active_list[row_index] = balloon_line_count
+
 
 def update() -> None:
     global screen, delta_time
@@ -34,6 +56,12 @@ def update() -> None:
 
             if balloon_collision:
                 balloon.set_active(False)
+                decrease_balloon_row_count(balloon_list.index(balloon))
+                
+                for balloon_row in range(0, balloon_row_count):
+                    if balloon_active_list[balloon_row] == 0:
+                        reactivate_balloon_row(balloon_row)
+                        
 
 
 def draw() -> None:
